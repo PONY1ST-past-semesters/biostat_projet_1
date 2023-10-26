fe8a258fbe91e09e2be6e27bb7a02f9efe0e1286
Wen, Zehai <r2d2wzh1@gmail.com>
2023-10-26 12:56:42 -0400

    changes

--- question_2_e.R
+++ question_2_e.R
@@ -7,7 +7,8 @@ attach(donnee)
 
 #On commence par le netoyage
 donnee$feeling_depressed <- as.factor(donnee$feeling_depressed)
-donnee$feeling_depressed[is.na(donnee$feeling_depressed)] <- as.factor(floor(runif(sum(is.na(donnee$feeling_depressed)), min = 1, max = 4.9999)))
+donnee$feeling_depressed[is.na(donnee$feeling_depressed)] <- as.factor(
+    floor(runif(sum(is.na(donnee$feeling_depressed)), min = 1, max = 4.9999)))
 
 donnee$smoking <- as.factor(donnee$smoking)
 
@@ -17,14 +18,15 @@ donnee$weight[is.na(donnee$weight)] <- mean(donnee$weight, na.rm = TRUE)
 donnee$time <- as.factor(donnee$time) #identiquement 1
 
 donnee$age <- as.numeric(donnee$age)
-donnee$age[is.na(donnee$age)] <- as.factor(floor(mean(donnee$age, na.rm = TRUE)))
+donnee$age[is.na(donnee$age)] <- as.factor(mean(donnee$age, na.rm = TRUE))
 
 donnee$sex <- as.factor(donnee$sex)
 donnee$sex[is.na(donnee$sex)] <- as.factor(floor(runif(sum(is.na(donnee$sex)), min = 1, max = 2.9999)))
 
-donnee$SES <- as.factor(donnee$SES)
-donnee$SES[is.na(donnee$SES)] <- as.factor(floor(runif(sum(is.na(donnee$SES)), min = 2, max = 10.9999)))
+donnee$SES <- as.numeric(donnee$SES)
+donnee$SES[is.na(donnee$SES)] <- as.numeric(floor(mean(donnee$SES, na.rm = TRUE)))
 
 attach(donnee)
 
-print(head(donnee, 30))
+modele_moindre_carre <- lm(weight ~ SES + age, data = donnee)
+print(summary(modele_moindre_carre))
